6699576d9e99bdaf22ae6261f8cd341b00fa1e81
曾文轩 <22373305@buaa.edu.cn>
2024-07-17 14:25:42 +0800

    v2 : add memory

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QLCDNumber, QHeaderView
 from qfluentwidgets import *
 import sys
+import os
 
 
 class Window(FluentWindow):
     def __init__(self):
         super(Window, self).__init__()
-        self.resize(1000, 600)
+        self.resize(1000, 700)
         self.setWindowTitle('Study Time')
         self.setWindowIcon(QIcon('icon-alram.svg'))
         # self.setWindowIcon(QIcon('favicon.ico'))
@@ -19,22 +20,12 @@ class Window(FluentWindow):
         layoutWidget = QtWidgets.QWidget(self)
         layout = QtWidgets.QGridLayout(layoutWidget)
         layoutWidget.setLayout(layout)
-        layoutWidget.setGeometry(QtCore.QRect(200, 100, 600, 400))
+        layoutWidget.setGeometry(QtCore.QRect(200, 100, 600, 500))
 
         self.start_button = PrimaryPushButton("start", layoutWidget)
         self.stop_button = PrimaryPushButton("stop", layoutWidget)
 
-
         self.timer = QtCore.QTimer(self)
-
-        # self.time_picker = TimeEdit()
-        # self.time_picker.setTime(QTime(0, 0, 0))
-        # self.time_picker.setReadOnly(True)
-        # self.time_picker.setDisplayFormat('hh : mm : ss')
-        # self.time_picker.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        # self.time_picker.setFont(QtGui.QFont())
-        # layout.addWidget(self.time_picker, 2, 1)
-
         self.lcd = QtWidgets.QLCDNumber(layoutWidget)
         self.lcd.setDigitCount(12)
 
@@ -44,19 +35,31 @@ class Window(FluentWindow):
 
         self.table = TableWidget(layoutWidget)
         self.table.setColumnCount(4)
-        # self.table.setRowCount(3)
         self.table.setHorizontalHeaderLabels(['开始时间', '结束时间', '时长', '总时间'])
         # self.table.horizontalHeader().setSectionResizeMode(QHeaderView.)
         # self.table.resizeColumnsToContents()
+
+        self.clear_button = PrimaryPushButton("clear", layoutWidget)
 
         layout.addWidget(self.start_button, 0, 3, 1, 4)
         layout.addWidget(self.stop_button, 1, 3, 1, 4)
         layout.addWidget(self.lcd, 2, 0, 2, 10)
         layout.addWidget(self.table, 4, 1, 2, 8)
+        layout.addWidget(self.clear_button, 6, 3, 1, 4)
 
         self.start_button.clicked.connect(lambda: click_start(self.timer))
         self.stop_button.clicked.connect(lambda: click_end(self.timer, self.table))
         self.timer.timeout.connect(lambda: refresh_time(self, self.lcd))
+        self.clear_button.clicked.connect(lambda: clear(self))
+
+        records = read_log()
+        total_time_str = None
+        for record in records:
+            add2table(self.table, *record)
+            total_time_str = record[-1]
+        if total_time_str is not None:
+            cumulative_time = QTime.fromString(total_time_str, 'hh : mm : ss . zzz')
+            display_lcd(cumulative_time, self.lcd)
 
 
 start_time: QTime = None
@@ -73,29 +76,56 @@ def click_end(timer: QtCore.QTimer, table: TableWidget):
     global start_time
     global cumulative_time
     if start_time is not None:
-        current_row_count = table.rowCount()
-        table.insertRow(current_row_count)
-        table.setItem(current_row_count, 0, QtWidgets.QTableWidgetItem(start_time.toString('hh : mm : ss')))
         end_time: QTime = QTime.currentTime()
         cumulative_time = cumulative_time.addMSecs(start_time.msecsTo(end_time))
-        table.setItem(current_row_count, 1, QtWidgets.QTableWidgetItem(end_time.toString('hh : mm : ss')))
-        table.setItem(current_row_count, 2, QtWidgets.QTableWidgetItem(QTime(0, 0, 0).addSecs(start_time.secsTo(end_time)).toString('hh : mm : ss')))
-        table.setItem(current_row_count, 3, QtWidgets.QTableWidgetItem(cumulative_time.toString('hh : mm : ss')))
+        str0 = start_time.toString('hh : mm : ss')
+        str1 = end_time.toString('hh : mm : ss')
+        str2 = QTime(0, 0, 0).addSecs(start_time.secsTo(end_time)).toString('hh : mm : ss')
+        str3 = cumulative_time.toString('hh : mm : ss . zzz')
+        add2table(table, str0, str1, str2, str3)
         # table.verticalScrollBar().setValue(table.verticalScrollBar().maximum())
+        write_log(str0 + ',' + str1 + ',' + str2 + ',' + str3)
         start_time = None
         timer.stop()
+
+
+def clear(window: Window):
+    global start_time
+    global cumulative_time
+    w = Dialog("Attention", "Are you sure to clear today's log?", window)
+    if w.exec():
+        start_time = None
+        cumulative_time = QTime(0, 0, 0, 0)
+        display_lcd(cumulative_time, window.lcd)
+        window.table.setRowCount(0)
+        today = datetime.date.today()
+        path = 'log/' + str(today) + '.log'
+        if os.path.exists(path):
+            with open(path, 'w') as file:
+                file.truncate(0)
+
+
+def add2table(table: TableWidget, *items):
+    current_row_count = table.rowCount()
+    table.insertRow(current_row_count)
+    for i, item in enumerate(items):
+        table.setItem(current_row_count, i, QtWidgets.QTableWidgetItem(item[0:12]))
 
 
 def refresh_time(window: Window, lcd: QLCDNumber):
     global cumulative_time
     global start_time
     window.time = cumulative_time.addMSecs(start_time.msecsTo(QTime.currentTime()))
-    if window.time.msec() % 10 != 0:
-        lcd.display(window.time.toString('hh:mm:ss.zz')[0:11])
-    elif window.time.msec() // 10 % 10 == 0:
-        lcd.display(window.time.toString('hh:mm:ss.zz') + '0')
+    display_lcd(window.time, lcd)
+
+
+def display_lcd(time: QTime, lcd: QLCDNumber):
+    if time.msec() % 10 != 0:
+        lcd.display(time.toString('hh:mm:ss.zz')[0:11])
+    elif time.msec() // 10 % 10 == 0:
+        lcd.display(time.toString('hh:mm:ss.zz') + '0')
     else:
-        lcd.display(window.time.toString('hh:mm:ss.zz'))
+        lcd.display(time.toString('hh:mm:ss.zz'))
 
 
 def timedelta2str(td: datetime.timedelta) -> str:
@@ -104,6 +134,29 @@ def timedelta2str(td: datetime.timedelta) -> str:
     minutes = (seconds % 3600) // 60
     seconds = seconds % 60
     return f"{int(hours):02} : {int(minutes):02} : {int(seconds):02}"
+
+
+def write_log(string):
+    today = datetime.date.today()
+    path = 'log/' + str(today) + '.log'
+    if os.path.exists(path):
+        with open(path, 'a') as file:
+            print(string, file=file)
+
+
+def read_log():
+    ret = []
+    today = datetime.date.today()
+    path = 'log/' + str(today) + '.log'
+    if not os.path.exists(path):
+        if not os.path.exists('log'):
+            os.mkdir('log')
+        open(path, 'a').close()
+    with open(path) as file:
+        # Dialog("log file error", "Log file corrupted, are you sure to clear today's logfile?", window)
+        for line in file:
+            ret.append(line.strip().split(','))
+    return ret
 
 
 if __name__ == "__main__":
